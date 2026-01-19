7cb2213aacdb2e8a31dc621f148825bac1a1719d
wqqqqq11 <2622643666@qq.com>
2026-01-19 13:28:16 +0800

    修改pipeline执行参数，更新.gitignore忽略规则，完善项目提交

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-from .pipeline import Pipeline
-from .service import app, start_service
-from .milvus_store import MilvusStore
-from .models import CLIPEmbedder, QwenLabeler
-from .metrics import MetricsCollector
-from .io_utils import DataLoader
-from .common import load_config, setup_logger
-
-__version__ = "1.0.0"
